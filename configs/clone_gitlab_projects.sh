24e33d94f76f5a29f305421c2648254ae9949c00
Yasin Demir <thelifeisnothing@gmail.com>
2024-07-08 11:56:57 +0200

    Update clone_gitlab_projects.sh

--- configs/clone_gitlab_projects.sh
+++ configs/clone_gitlab_projects.sh
@@ -4,9 +4,16 @@
 GITLAB_GROUP="success-factory-development"  # URL'den alınan grup adı
 GITLAB_PRIVATE_TOKEN="glpat-WsFRAWDYwLcLnKQPzzBs"  # Oluşturduğunuz kişisel erişim tokenı
 
+# Projeleri indireceğimiz dizin
+DESTINATION_DIR="$HOME/Desktop/Projects"
+
 # Proje listesini almak için GitLab API'sini kullan
-echo "Projeleri alınıyor..."
 projects=$(curl --header "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" "https://gitlab.com/api/v4/groups/$GITLAB_GROUP/projects?per_page=100" | jq -r '.[].ssh_url_to_repo')
+
+# Hedef dizinin var olup olmadığını kontrol et, yoksa oluştur
+if [ ! -d "$DESTINATION_DIR" ]; then
+  mkdir -p "$DESTINATION_DIR"
+fi
 
 # Projeleri klonla
 if [ -z "$projects" ]; then
@@ -17,7 +24,7 @@ fi
 echo "Projeler klonlanıyor..."
 for project in $projects; do
   echo "Klonlanıyor: $project"
-  git clone "$project" || { echo "Klonlama hatası: $project"; exit 1; }
+  git clone "$project" "$DESTINATION_DIR/$(basename $project .git)" || echo "Klonlama hatası: $project"
 done
 
-echo "Tüm projeler başarıyla klonlandı."
+echo "Tüm projeler klonlama işlemi tamamlandı."
